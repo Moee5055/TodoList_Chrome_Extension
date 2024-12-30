a83bc019b9896e3c68324b90b9b2e880a6e40da3
Moee5055 <kshitij.moee55@gmail.com>
2024-12-30 22:32:08 +0545

    B: configure tailwind in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-
 const App = () => {
   return (
     <div>
-
+      <h2 className="text-lg text-red-600">hllo</h2>
+      <input className="outline outline-gray-200 rounded-sm px-2 py-1"/>
     </div>
   )
 }
