d1d5dba6e3cb5547e52969f2de32ed45fd757d82
Moee5055 <kshitij.moee55@gmail.com>
2024-12-31 23:34:31 +0545

    height adjusted in content.css: styling on heading

--- src/App.tsx
+++ src/App.tsx
@@ -74,7 +74,7 @@ useEffect(() => {
       <div>
         <TodoInput onAdd={addTodo} />
         <div className='flex justify-between items-center pr-2 mb-4'>
-          <h2 className="text-xl font-bold">TODO <ClipboardCheck /></h2>
+          <h2 className="text-xl flex font-extrabold gap-2">Today Task <ClipboardCheck /></h2>
           <button
             className="px-2 py-1 bg-red-500 text-white font-semibold rounded-lg shadow-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 focus:ring-offset-2 transition duration-300" onClick={() => {
               setTodos([])
